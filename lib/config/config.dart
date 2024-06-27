b9bb7a529c570a578504d382d07f6f9226b92b18
Ricardo <a1121120071@utch.edu.mx>
2024-06-26 21:53:37 -0600

    correcciones v2

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,16 @@
 library config.globals;
 
+export 'package:flutter/cupertino.dart';
+export 'package:flutter/material.dart' hide RefreshCallback;
+
+
+export 'package:gto/pantallas/app.dart';
+export 'package:gto/pantallas/controles.dart';
+export 'package:gto/pantallas/home.dart';
+export 'package:gto/widget/celda.dart';
+
+
+
 enum estados { vacio, cruz, circulo }
 
 List<estados> tablero = List.filled(9, estados.vacio);
